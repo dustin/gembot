97b64acd9cc776f0dc3bc8c434542c9d0c05a8e5
Dustin Sallings <dustin@spy.net>
2013-04-16 23:14:06 -0700

    Actually processing a buy.

--- gembot.go
+++ gembot.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"strings"
+	"time"
 
+	"github.com/dustin/go.bitcoin"
 	"github.com/dustin/goquery"
 )
 
 const worthPrefix = "It is worth "
 
-type State struct {
-	Value string
+var threshold = bitcoin.Amount(bitcoin.SatoshisPerBitcoin / 10)
+
+var siteread = flag.String("siteread", "http://bitcoingem.com/", "The site to monitor")
+var sitebuy = flag.String("sitebuy", "http://bitcoingem.com/calls/create_new_address3.php",
+	"URL to do the purchasing")
+var recvaddr = flag.String("recvaddr", "", "A receive address for payouts")
+var fromacct = flag.String("fromacct", "", "Sending account name")
+var checkInterval = flag.Duration("interval", time.Minute, "How frequently to check")
+var comment = flag.String("comment", "", "comment on your transaction")
+
+var bcServer = flag.String("bitcoin", "http://localhost:8333/", "Bitcoind")
+var bcUser = flag.String("bcuser", "someuser", "Bitcoin user")
+var bcPass = flag.String("bcpass", "somepass", "Bitcoin password")
+
+func init() {
+	flag.Var(&threshold, "maxbid", "Maximum bid we'll place")
 }
+
+type State struct {
+	IsMine bool
+	Value  bitcoin.Amount
+}
+
+var unknownData = errors.New("I don't recognize the data")
+
+var bc *bitcoin.BitcoindClient
 
 func must(err error) {
 	if err != nil {
@@ -28,15 +59,84 @@ func parse(r io.Reader) (State, error) {
 		return rv, err
 	}
 	txt := g.Find("h2").Text()
-	log.Printf("Got %q", txt)
 
 	if txt[:len(worthPrefix)] == worthPrefix {
 		parts := strings.SplitN(txt[len(worthPrefix):], " ", 2)
-		rv.Value = parts[0]
+		rv.Value, err = bitcoin.AmountFromBitcoinsString(parts[0])
+	} else {
+		err = unknownData
 	}
 
-	return rv, nil
+	rv.IsMine = strings.Contains(txt, *recvaddr)
+
+	return rv, err
+}
+
+func buy(amt bitcoin.Amount) error {
+	res, err := http.PostForm(*sitebuy, url.Values{"address": {*recvaddr}})
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return fmt.Errorf("Failed to get payout address: %v", res.Status)
+	}
+	resdata, err := ioutil.ReadAll(io.LimitReader(res.Body, 80))
+	if err != nil {
+		return err
+	}
+	ress := strings.TrimSpace(string(resdata))
+
+	x, err := bc.ValidateAddress(ress)
+	if err != nil {
+		return err
+	}
+	if !x.Isvalid {
+		return fmt.Errorf("Returned an invalid address:  %v", ress)
+	}
+
+	var txn string
+	if *fromacct == "" {
+		txn, err = bc.SendToAddress(x.Address, amt, *comment, "")
+	} else {
+		txn, err = bc.SendFrom(*fromacct, x.Address, amt, -1, *comment, "")
+	}
+
+	if err == nil {
+		log.Printf("Sent txn %v", txn)
+	}
+
+	return err
+}
+
+func checkSite() {
+	res, err := http.Get(*siteread)
+	must(err)
+	defer res.Body.Close()
+	st, err := parse(io.LimitReader(res.Body, 4096))
+	must(err)
+	log.Printf("Current state:  %+v", st)
+
+	if st.IsMine {
+		log.Printf("I already seem to own it")
+		return
+	}
+
+	if st.Value <= threshold {
+		log.Printf("Hey, we'll give that a bid!")
+		err := buy(st.Value)
+		must(err)
+	}
 }
 
 func main() {
+	flag.Parse()
+
+	if *recvaddr == "" {
+		log.Fatalf("You need to supply a receive addr")
+	}
+
+	bc = bitcoin.NewBitcoindClient(*bcServer, *bcUser, *bcPass)
+
+	checkSite()
 }
--- gembot_test.go
+++ gembot_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/dustin/go.bitcoin"
 )
 
 func parseFile(t *testing.T, fn string) State {
@@ -20,7 +22,10 @@ func parseFile(t *testing.T, fn string) State {
 
 func TestCurrentValue(t *testing.T) {
 	st := parseFile(t, "samples/normal.html")
-	exp := "1.82"
+	exp, err := bitcoin.AmountFromBitcoinsString("1.82")
+	if err != nil {
+		t.Fatalf("Error parsing expected amount:  %v", err)
+	}
 	if st.Value != exp {
 		t.Errorf("Expected value %q, got %q", exp, st.Value)
 	}
