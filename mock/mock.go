a530f1223bae38bbed83ff1634302e230e068378
Dustin Sallings <dustin@spy.net>
2013-04-16 23:13:47 -0700

    A mock server.

---
+++ mock/mock.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func buyHandlerSuccess(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("mj5rLRjgu75mmVbVKBshfZAA4qktDkzKLZ"))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../samples/mine.html")
+}
+
+func main() {
+	http.HandleFunc("/buy", buyHandlerSuccess)
+	http.HandleFunc("/", rootHandler)
+
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
